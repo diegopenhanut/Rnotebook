97c7869e0b390dd44e7abded338e3d70b844e166
diegopenhanut <diegopenhanut@gmail.com>
2014-08-24 00:03:25 -0500

    merge intake data with food composition.

--- firstScriptPOF.R
+++ firstScriptPOF.R
@@ -28,7 +28,32 @@ View (consumo[1:10,])
 consumo$COD_UF<-apply(consumo, 1 , function(aa) rotulos$COD_UF$rotulo[match (aa["COD_UF"], rotulos$COD_UF$valor)])
 consumo$COD_UF<-as.factor(consumo$COD_UF)
 
-consumo$COD_ITEM<-apply(consumo, 1 , function(aa) rotulos$COD_ITEM$rotulo[match (aa["COD_ITEM"], rotulos$COD_ITEM$valor)])
-consumo$COD_ITEM<-as.factor(consumo$COD_ITEM)
+#consumo$COD_ITEM<-apply(consumo, 1 , function(aa) rotulos$COD_ITEM$rotulo[match (aa["COD_ITEM"], rotulos$COD_ITEM$valor)])
+#consumo$COD_ITEM<-as.factor(consumo$COD_ITEM)
+
 #' save first 1000 lines
 write.csv(consumo[1:1000,], "~/Google Drive/pof - aalane/POF-R/consumo-1000linhas.csv")
+
+#' tabela completa URL
+#' ftp://ftp.ibge.gov.br/Orcamentos_Familiares/Pesquisa_de_Orcamentos_Familiares_2008_2009/Tabelas_de_Composicao_Nutricional_dos_Alimentos_Consumidos_no_Brasil/tabelacompleta.zip
+compCentesimal<-read.table(file = "~/Google Drive/pof - aalane/tabelacompleta.txt",
+  skip = 3, sep = "\t", header = TRUE, fileEncoding =  "latin1", na.strings="-")
+#' drop blank fields
+compCentesimal<-compCentesimal[,1:43]
+#' merge
+compInd <- merge(x = consumo, y = compCentesimal, by.x = c("COD_ITEM", "COD_PREPARACAO"),
+  by.y = c("CîDIGO.DO.ALIMENTO", "CîDIGO............DA.PREPARA.ÌO"), all.x = TRUE, all.y = FALSE)
+
+View(compInd[1:10,])
+
+df <- data.frame(data.frame(unlist(compInd[1:10,]), nrow=67, byrow=T))
+
+#require("plyr")
+
+
+compInd<-as.data.frame(compInd)
+
+#' basic math
+
+as.numeric(compInd$QTD_FINAL)/100*as.numeric(compInd$ENERGIA..kcal.)
+lala<-as.vector(compInd[1:10,31:length(compInd)])*as.numeric(compInd$QTD_FINAL)
